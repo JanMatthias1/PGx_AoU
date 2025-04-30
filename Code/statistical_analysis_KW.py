1379d30574ed45dc208ef3a14c9c08c5c0ba3dfd
Jan Matthias <114141337+JanMatthias1@users.noreply.github.com>
2025-04-30 14:11:31 -0700

    Update statistical_analysis_KW.py

--- Code/statistical_analysis_KW.py
+++ Code/statistical_analysis_KW.py
@@ -149,75 +149,74 @@ for subset in ["all", "White", "Black or African American","Hispanic or Latino" 
 
     elif subset == "all":
         df_race = df.copy()
-      
+
     for j, CYP_gene in enumerate(["CYP2D6", "CYP2C19", "CYP2C9", "CYP2B6", "CYP3A5"]):
-    
+
         print(CYP_gene, "CYP____________")
-        
+
         # --- Filtering for CYP and dropping null values
-        df_copy=df.copy()
+        df_copy = df_race.dropna(subset=[CYP_gene]).copy()
         df_copy["metabolizer_type"] = df_copy[CYP_gene]
-        df_copy = df_copy.dropna(subset=["metabolizer_type"])
-        
+
         # Track tested drugs to avoid duplicate tests
         tested_drugs = set()
-    
+
         for CYP_drug, drug_list in drug_dictionary.items():
             
             for drug_name in drug_list:
-                
+
                 print(f"Before {CYP_gene} with {drug_name} (CYP_drug: {CYP_drug})")
-                
+
                 if drug_name in tested_drugs:
                     print(f" ---------- Skipping {drug_name}, {CYP_drug} already tested -------------")
                     continue
-                    
+
                 # if the drug_name has already been tested, no need to duplicate
                 tested_drugs.add(drug_name)
-                
+
                 CYP_drug_modified = find_metabolizing_enzyme(drug_name, CYP_gene, CYP_drug, drug_to_enzymes)
-    
+
                 print(f"Testing {CYP_gene} with {drug_name} (CYP_drug: {CYP_drug_modified})")
-    
+
                 # ---------------- DRUG extraction from data set -------------
                 pattern = fr'\b{drug_name}\b'
-    
+
                 # Create a dataframe of patients on the drug
                 df_first_drug = df_copy[df_copy['drug_name'].str.contains(pattern, case=False, na=False)].copy()
-            
+
                 # ------------- filter for at least 5 observations
                 # Optimize filtering using .value_counts()
                 valid_persons = df_first_drug['person_id'].value_counts()
                 valid_persons = valid_persons[valid_persons >= 5].index
                 df_filtered = df_first_drug[df_first_drug['person_id'].isin(valid_persons)]
-    
+
                 # Apply per person
                 final_rows = df_filtered.groupby("person_id").apply(process_person).reset_index(drop=True)
-    
+
                 # Now compute mean dose per person from these cleaned 5 observations
                 mean_dose_per_day_five = final_rows.groupby("person_id")["dose_per_day"].mean().round(10).reset_index()
-    
+
                 # Columns to merge
                 merge_cols = ["person_id", "metabolizer_type", "sex_at_birth", "BMI", "age_calculated_years"] + pca_columns
-        
+
                 # Merge additional information     
                 mean_dose_per_day = mean_dose_per_day_five.merge(df_first_drug[merge_cols].drop_duplicates(subset="person_id"),
                                                                  on="person_id", how="left")
-    
+
                 # 2) ---------- REMOVING OUTLIERS FROM THE OBSERVATIONS 
                 mean_dose_per_day = mean_dose_per_day[mean_dose_per_day["dose_per_day"] != 0]
                 # Drop duplicates again (ensure only last observation per person is kept)
-                
+
                 mean_dose_per_day= remove_outliers(mean_dose_per_day, column="dose_per_day", lower_quantile=0.25, 
                                                    upper_quantile=0.75, iqr_multiplier=1.5)
-                
+
                 mean_dose_per_day = mean_dose_per_day.sort_values("person_id").reset_index(drop=True)
-                
-                mean_dose_per_day[["drug_name", "gene_name", "drug_CYP"]] = drug_name, CYP_gene, CYP_drug_modified
-                
+
+                mean_dose_per_day[["drug_name", "gene_name", "drug_CYP", "subset"]] = drug_name, CYP_gene, CYP_drug_modified, subset
+
                 if CYP_gene == "CYP3A5" or CYP_gene == "CYP2C9":
                     mean_dose_per_day['metabolizer_group'] = mean_dose_per_day['metabolizer_type']
-                
+
                 else: 
                     # Group metabolizer types
                     mean_dose_per_day['metabolizer_group'] = mean_dose_per_day['metabolizer_type'].replace({
@@ -226,58 +225,58 @@ for subset in ["all", "White", "Black or African American","Hispanic or Latino" 
                         "Rapid": "Rapid/Ultrarapid",
                         "Ultrarapid": "Rapid/Ultrarapid"
                     })
-                
+
                 participants = len(mean_dose_per_day)
-                
+
                 # Convert the unique metabolizer groups list to a string before storing
                 metabolizer_groups_str = ", ".join(map(str, mean_dose_per_day["metabolizer_group"].unique()))
-                
+
                 check_grouped_data = mean_dose_per_day.groupby("metabolizer_group")["dose_per_day"]
                 has_variation = all(group.std() > 0 for _, group in check_grouped_data)
-                
+
                 if participants >= 50 and has_variation:
-                    
+
                     # Compute summary statistics **after filtering**
                     if not mean_dose_per_day.empty:
                         summary_stats= update_summary_statistics(mean_dose_per_day, CYP_drug_modified, CYP_gene, drug_name, summary_stats)
-            
+
                     # ------- Covariate adjusted Drug Dosage 
                     mean_dose_per_day, model = calculate_adjusted_dosage(mean_dose_per_day, pca_columns) 
-    
+
                     # Store p-values and beta coefficients
                     feature_importance = store_feature_importance(model, CYP_drug_modified, CYP_gene, drug_name, pca_columns, 
                                                                   feature_importance)
-                
+
                     data_to_export = pd.concat([data_to_export, mean_dose_per_day], ignore_index=True)
-                    
+
                     # Count unique metabolizer groups
                     unique_groups = mean_dose_per_day["metabolizer_group"].nunique()
-                    
+
                     if unique_groups == 3:
                         # Perform Kruskal-Wallis test for three groups
                         p_value, statistic, test_type = perform_kruskal_test(mean_dose_per_day.groupby("metabolizer_group"), 3)
-    
+
                     else:
                         # If there are not exactly 2 or 3 groups, skip the test
                         p_value = "Skipped"
                         statistic = "Skipped"
                         test_type = "Skipped"
-                    
-    
+
+
                     # Append to data frame
                     data_stat.loc[len(data_stat)] = [CYP_drug_modified, CYP_gene, drug_name, p_value, statistic,
                                                      len(mean_dose_per_day["metabolizer_group"].unique()), 
                                                      participants, test_type,
-                                                     metabolizer_groups_str]
-                
+                                                     metabolizer_groups_str, subset]
+
                 else:
                     p_value = "Skipped"  # Not enough data for the test
                     statistic = "Skipped"
-                    
+
                     number_of_metabolizer= "Skipped"
                     data_stat.loc[len(data_stat)] = [CYP_drug_modified, CYP_gene, drug_name, p_value, statistic, 
                                                         number_of_metabolizer, participants, "Skipped",
-                                                        metabolizer_groups_str]
+                                                        metabolizer_groups_str, subset]
                 
 
  #----------------- SIGNIFICANT P-VALUES ----------------
